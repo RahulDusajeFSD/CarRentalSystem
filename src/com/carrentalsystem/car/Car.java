2963916c083c4176b87ec88a4664f6a842e40537
Rahul Dusaje <60280577+RahulDusajeFSD@users.noreply.github.com>
2024-12-02 23:46:43 -0500

    Initial Commit

---
+++ src/com/carrentalsystem/car/Car.java
@@ -0,0 +1,69 @@
+package com.carrentalsystem.car;
+
+public class Car {
+
+	private int Id;
+	private String brand;
+	private String model;
+	private String color;
+	private int year;
+	private boolean available;
+	private double price;
+
+	public Car() {
+
+	}
+
+	public int getId() {
+		return this.Id;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void getModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
---
+++ src/com/carrentalsystem/user/User.java
@@ -0,0 +1,53 @@
+package com.carrentalsystem.user;
+
+public abstract class User {
+	
+	private int Id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private String password;
+	public int getId() {
+		return Id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public User() {
+		
+	}
+	
+	
+	public abstract void showList();
+
+}
